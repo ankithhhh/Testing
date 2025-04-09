afbe65227637a9d1d2ed01bf999227f9e9f512f7
ankithhhh <114284384+ankithhhh@users.noreply.github.com>
2025-04-09 17:42:13 +0530

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,25 +1,18 @@
-package runners;  // Ensure this matches your package structure
+package runners;
 
-import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/Feature/",  // Update this path to where your feature files actually are
+    features = "src/test/resources/Feature",
     glue = {"Stepdefination", "utils"},
     plugin = {"pretty", "html:target/cucumber-reports"},
-    dryRun = false,  // Change to false to actually run the tests
+    dryRun = false,
     monochrome = true
 )
 public class TestRunner {
+    // Empty class body
 }
-
-
-public class TestRunner {
-}
-
-
-
 
