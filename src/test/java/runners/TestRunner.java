d4f1034535efe036efba3f7d458e7200b779566f
ankithhhh <114284384+ankithhhh@users.noreply.github.com>
2025-04-09 17:01:21 +0530

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,9 +6,10 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/Feature", // ✅ This matches your actual path
-    glue = {"Stepdefination", "utils"}, 
-    plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"},
+    features = "src/test/resources/Feature/LocalApi.feature",
+    glue = {"Stepdefination", "utils"},
+    plugin = {"pretty"},
+    dryRun = true,
     monochrome = true
 )
 
