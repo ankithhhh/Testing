4a9949fb3366bb27b0996e5cb293b2382a1397e0
ankithhhh <114284384+ankithhhh@users.noreply.github.com>
2025-04-10 10:23:50 +0530

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
+    features = "src/test/resources/Feature",
     glue = {"stepdefinitions", "utils"},  // Changed from "Stepdefination" to "stepdefinitions"
     plugin = {"pretty", "html:target/cucumber-reports"},
     dryRun = false,
